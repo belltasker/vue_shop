ef18d74a9de398f8e9753a70e686ceb0e79df366
belltasker <belltasker@hotmail.com>
2020-02-12 16:45:47 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>首页</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../../assets/logo.png" alt="图片无法显示" height="60px" />
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h2>后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+            <a class="loginout" href="#">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +29,28 @@ export default {}
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
+
+.header {
+  background-color: #b3c0d1;
+}
+
+.aside {
+  background-color: #d3dce6;
+}
+
+.main {
+  background-color: #e9eef3;
+}
+
+.middle {
+  text-align: center;
+}
+
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
