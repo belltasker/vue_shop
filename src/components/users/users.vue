0bda6e545b804d503a380c15866bd947de481e0e
belltasker <belltasker@hotmail.com>
2020-02-13 15:05:32 +0800

    列表-面包屑-搜索框-表格-获取数据

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,10 +1,89 @@
 <template>
-  <div>用户</div>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="searchRow">
+      <el-col :span="8">
+        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </el-col>
+      <el-col :span="16">
+        <el-button type="success">添加按钮</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" label="#" width="60"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="80"></el-table-column>
+      <el-table-column prop="address" label="邮箱"></el-table-column>
+      <el-table-column prop="address" label="电话"></el-table-column>
+      <el-table-column prop="address" label="日期"></el-table-column>
+      <el-table-column prop="address" label="创建时间"></el-table-column>
+      <el-table-column prop="address" label="用户状态"></el-table-column>
+      <el-table-column prop="address" label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      query: '',
+      pagenum: 1,
+      pagesize: 10,
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  methods: {
+    async getUserList() {
+      const token = localStorage.getItem('token')
+      this.$http.defaults.headers.common['Authorization'] = token
+
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
+      )
+      console.log(res)
+    }
+  },
+  mounted() {
+    this.getUserList()
+  }
+}
 </script>
 
 <style>
+.box-card {
+  height: 100%;
+}
+.inputSearch {
+  width: 50%;
+}
+.searchRow {
+  margin-top: 20px;
+}
 </style>
